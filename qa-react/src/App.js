da14506ee60b0cef81e7db8c599734600a1ecd78
AlexBrochu <alexandre.brochu.1@ens.etsmtl.ca>
2018-10-06 16:03:15 -0400

    Init router and auth0

--- qa-react/src/App.js
+++ qa-react/src/App.js
@@ -1,12 +1,18 @@
 import React, { Component } from 'react';
+import {Route} from 'react-router-dom';
 import NavBar from './NavBar/NavBar';
+import Callback from './Callback';
+import Questions from './Questions/Questions';
+import Question from './Question/Question';
 
 class App extends Component {
   render() {
     return (
       <div>
         <NavBar/>
-        <p>Work in progress.</p>
+        <Route exact path='/' component={Questions}/>
+        <Route exact path='/question/:questionId' component={Question}/>
+        <Route exact path='/callback' component={Callback}/>
       </div>
     );
   }
